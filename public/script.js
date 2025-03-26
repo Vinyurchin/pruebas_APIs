99c46f4f5762e0fbb1421b0e771079ecf1b15970
Vinyurchin <diegoc.g56@gmail.com>
2025-03-25 18:43:21 -0600

    Cambiado el diseño de la página a un modo oscuro

--- public/script.js
+++ public/script.js
@@ -2,9 +2,8 @@ const form = document.getElementById('userForm');
 const responseMessage = document.getElementById('responseMessage');
 
 form.addEventListener('submit', async (event) => {
-  event.preventDefault(); // Evita que el formulario recargue la página
+  event.preventDefault();
 
-  // Obtén los datos del formulario
   const data = {
     username: form.username.value,
     email: form.email.value,
@@ -12,7 +11,6 @@ form.addEventListener('submit', async (event) => {
   };
 
   try {
-    // Envía los datos a la API
     const response = await fetch('http://localhost:3001/users', {
       method: 'POST',
       headers: {
@@ -25,7 +23,7 @@ form.addEventListener('submit', async (event) => {
       const result = await response.json();
       responseMessage.textContent = 'Usuario registrado con éxito: ' + result.username;
       responseMessage.style.color = 'green';
-      form.reset(); // Limpia el formulario
+      form.reset();
     } else {
       responseMessage.textContent = 'Error al registrar el usuario.';
       responseMessage.style.color = 'red';
